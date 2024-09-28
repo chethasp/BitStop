bf32cdc9be801e4433f285bd0c0eab76b155d75c
Thanish24 <boggulathanish@gmail.com>
2024-09-28 05:26:35 -0400

    added creating a graph with dummy weights

--- bus_routes/graph_operations.py
+++ bus_routes/graph_operations.py
@@ -1,57 +1,44 @@
+import csv
+
 class Node:
-    def __init__(self, address, weight=1):
-        """
-        Initializes a new node with an address and a busyness weight.
-        
-        Parameters:
-        - address (str): Unique identifier for the node (e.g., address or name).
-        - weight (int or float): Busyness or importance of the node. Default is 1.
-        """
+    def __init__(self, name, address, latitude, longitude, weight=0):
+        self.name = name
         self.address = address
         self.weight = weight
-        self.connections = {}  # Dictionary to hold connections {node: travel_time}
-        
-    def add_connection(self, node, weight):
-        """
-        Adds a connection from this node to another node.
-        
-        Parameters:
-        - node (Node): The node to which this node is connected.
-        - weight (int or float): Travel time or weight of the edge connecting the nodes.
-        """
-        self.connections[node] = weight
-
+        self.latitude = latitude
+        self.longitude = longitude
+    
     def __str__(self):
-        """
-        Returns a string representation of the node and its connections.
-        """
-        connections_str = ', '.join([f"{neighbor.address} ({weight})" for neighbor, weight in self.connections.items()])
-        return f"Node({self.address}, Weight={self.weight}, Connections=[{connections_str}])"
+        return "Node info: {n}, {a}, {w}".format(n=self.name, a=self.address, w=self.weight)
+    
+class Graph:
+    def __init__(self):
+        self.nodes = {} # key: name, value: node object
+        self.edges = {} # adjacency list for edges
 
-    def get_coordinates():
-        # Example list of coordinates
-        return [
-            (40.7128, -74.0060),  # New York City
-            (34.0522, -118.2437), # Los Angeles
-            (41.8781, -87.6298),  # Chicago
-        ]
+    def add_node(self, name, address, weight, latitude, longitude):
+        self.nodes[name] = Node(name, address,latitude, longitude, weight)
+        self.edges[name] = {}
 
-# Example usage
-# Creating nodes
-node_a = Node("A", weight=5)
-node_b = Node("B", weight=2)
-node_c = Node("C", weight=8)
+    def add_edge(self, name1, name2, weight=1):
+        self.edges[name1][name2] = weight
+        self.edges[name2][name1] = weight
 
-# Adding connections
-node_a.add_connection(node_b, 3)
-node_a.add_connection(node_c, 5)
-node_b.add_connection(node_c, 1)
+    def get_node(self, name):
+        return self.nodes[name]
+    
+    def get_edge(self, name1, name2):
+        return self.edges[name1][name2]
 
-# Display the nodes and their connections
-print(node_a)
-print(node_b)
-print(node_c)
+graph = Graph()
 
-#Node(A, Weight=5, Connections=[B (3), C (5)])
-#Node(B, Weight=2, Connections=[C (1)])
-#Node(C, Weight=8, Connections=[])
+with open('static/foot_traffic_sites.csv', mode ='r')as file:
+    csvFile = csv.reader(file)
+    for lines in csvFile:
+        if (lines[0] != 'venue_name'):
+            graph.add_node(lines[0], lines[1], 1, lines[2], lines[3])
+            for node in graph.nodes:
+                graph.add_edge(lines[0], graph.nodes[node].name, 1)
+
+print(graph.nodes.keys())
+print(graph.edges.values())
